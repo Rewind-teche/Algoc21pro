a9e2da538fd959d0205411991c5c69de95e765bb
Rewind-teche <67373977+Rewind-teche@users.noreply.github.com>
2020-07-04 16:54:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var bullet, speed, weight
+var  wall,thickness
+
+function setup() {
+  createCanvas(1600,400);
+  speed = random(223,321)
+  weight = random(30,52)
+  thickness = random(23,83)
+
+  bullet = createSprite(50,200, 100, 10);
+  bullet.shapeColor = color(255,178,102);
+  bullet.velocityX = speed;
+
+  wall = createSprite(1200,200,thickness,200);
+  wall.shapeColor = color(80,80,80);
+}
+
+function draw() {
+  background(255,255,255);  
+
+  if(collided(bullet,wall))
+  {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness)
+
+    if(damage<10)
+    {
+      wall.shapeColor = color(255,0,0)
+    }
+
+    if(damage>10)
+    {
+      wall.shapeColor = color(0,255,0);
+    }
+  }
+  drawSprites();
+}
+
+function collided(abullet,awall)
+{
+  bulletRightEdge = abullet.x + bullet.width;
+  wallLeftEdge = awall.x;
+  if (bulletRightEdge >= wallLeftEdge)
+  {
+    return true
+  }
+  return false;
+}
